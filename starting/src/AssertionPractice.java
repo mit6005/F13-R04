f5e4d7feb7aa63be774d7c7b217e97e1a70b21f7
MikeSalvato@gmail.com <MikeSalvato@gmail.com>
2013-09-16 23:07:09 -0400

    added assertiosn

---
+++ starting/src/AssertionPractice.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.Iterator;
+
+class CircularList {
+
+    // Construct an empty circular list with fixed number of elements
+    public CircularList(int elements) {
+    }
+
+    // Insert el as a new first element
+    public void InsertFirst(Object el) {
+
+    }
+
+    // Insert el as a new last element
+    public void InsertLast(Object el) {
+
+    }
+
+    // Delete my first element
+    public void DeleteFirst() {
+
+    }
+
+    // Delete my last element
+    public void DeleteLast() {
+
+    }
+
+    // Return the first element in the list
+    Object RetrieveFirst() {
+
+        return null;
+    }
+
+    // Return the last element in the list
+    Object RetrieveLast() {
+
+        return null;
+    }
+}
